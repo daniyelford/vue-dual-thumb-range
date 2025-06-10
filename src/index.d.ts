d94c397658cb236012bd1cd021f879c304e7d739
daniyelford <29danialfrd69@gmail.com>
2025-06-10 06:17:02 +0330

    published

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,5 @@
 import type { Plugin } from 'vue'
 import type { Component } from 'vue'
 export const CustomRange: Component
-declare const VueRange: Plugin
-export default VueRange
+declare const VueRangeDual: Plugin
+export default VueRangeDual
